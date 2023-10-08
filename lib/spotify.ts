68666b7d1d1f6ca2638a87b9c232f9b9cdf0cb8c
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-10-08 21:31:23 +0700

    Update spotify.ts

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -10,7 +10,7 @@ const TOP_TRACKS_DEFAULT_PARAMS = {
   limit: "5",
   offset: "0",
 };
-const TOKEN_ENDPOINT = `https://alecchen.dev/spotify-refresh-token`;
+const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`;
 
 const getAccessToken = async () => {
   const response = await fetch(TOKEN_ENDPOINT, {
@@ -20,7 +20,7 @@ const getAccessToken = async () => {
       "Content-Type": "application/x-www-form-urlencoded",
     },
     body: new URLSearchParams({
-      grant_type: "AQDjweIoiATYj9wxFyuAe67HDITj13WI_ZytSivHXDNJmQuATuO86XbWCY-dBSI31GcCTKSWN4B3yfGm81yYfiF9ud7AUnqwhPcwcyc9WvDJcwchZfApsCl7G2q2pI0whpTn6D4FKC4S14immasi1JAozHh0uDQ0a3GOsxxVRP44Hyz5oZ0WP19nDIr9m8htMvD0W1OfosN2Vt4gZQMDVPBO94kMHseJQxtsq1Vrp_S8rUtidzC5v4w6i9-_5qEvpkluEWOKLCJQUKt80oyHgf9eDjZ85kXYNMUHA02UqytrELFsK4PsIlX5NVwOzWfzQErMqDeDnxuoLS5-5gEqrIVDmiV8FuSXXeSYciqYDIAvTbzKzNPOZ6Y11mxs_uas27g5zDUS6ty3ZVgM_d0nXat82gFytyYdXDCwslrKnWOEk_3A3vyGVFp2CkMqCvm-DP7b2riRH6qaBVfYlqlduThmL0-mnLOmk4dSpX9efc71xvY9NTci269hcF2N2MuXmXY8WXbbzTWNfD8Xs6_HFNqKhWy9w8RNE7KYYsYKtw0CGsdUw7KBp6qB-X26IaRP-jEkB_J_4LKkOTZQwGrMpoPc5efhC0eppw03Y7ku6lZ8sUehOuqQz-aToQG-ElQThyXQCCvAKuDrbNEQIfvTs1xi_H8Teeh7mVWuiSQU4Ju3X950b1eqNP4mqHug7NZb-R-igmgcI82wX9ptwztZwhV9vHd0afSYHsv6vHhHVBU",
+      grant_type: 'client_credentials',
       refresh_token,
     }),
   });
