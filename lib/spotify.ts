183b458c11e4bf7e397f0839c850691cad0dd7ce
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-10-08 21:49:56 +0700

    Update spotify.ts

--- lib/spotify.ts
+++ lib/spotify.ts
@@ -21,7 +21,7 @@ const getAccessToken = async () => {
     },
     body: new URLSearchParams({
       grant_type: 'refresh_token',
-      refresh_token,
+      refresh_token: `AQDUNCIIXgukPy4XEqSpGjgxSz18yGcHsKFI-90F7eiTbhG66z9YGw8fyKW3lduA2m-OjYNNbW-XdS-HitUVE3ZP44aNcW4XFsuxMOzxW9UXnZqX_jg-eGeFH30iwAU50rrdTJiBJod5WRcXme4y4XKX9E4b50mIJl9UHN7lZYA_k4ocAVeY7h6Bpo5wmgtkXK9WRmdLtGaRKSrJnA3vt42DOunvyT44QcI_BkGOcQndMVxlzfuBAerGLq5-MmLfoy8E1bA1lfBGiyzkk9vb2PxwTckNgRFAqqtvWBMswTVRcaA29Ew_BdefPg9yKPWRKpSVpU01U0aT7zJ6oyOsS293VcjBOf2sXmB9n9hueYZHB3jdkWboOEWpB0XZseVWRBmSSbB6ItI1OkGVwJdgF1EhVEdA0QH45fRmczaKqBaq7ph7pBLB-yHyDbjcH9gko3a7RDK1afkZY4z3P49_xui1oJYoCDATwDaACdZl67B69D6r7Yhl9xRx1OFhpwY3IrGxh3CMf4l0eXzrJpJwpSPO102KrnQ16TWOuOPFgUNGm6L50WoFFgkmIwfhCUWmAgZfQFKQQhrd1RQbomGKA9ZZ9_T1MYkk1HvZ3cstn6IAzbWocsdkfGqkHwTuv7gEvOD3SNozDC0F-0tsLmRBaEbIc08Z4LiOKBWm6eg8D420v5x-RV_JVQV2g6VI_Q9Dk_fzgzzUZ8GgT23arOUsBmPsR7FT0P8qhr81uMeST5E`,
     }),
   });
   return response.json();
