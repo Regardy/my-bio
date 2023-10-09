b4bccee55418e166ba394349567f75c4f21ae23c
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-10-09 13:18:40 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -24,7 +24,9 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head />
         <body>
-          <audio id="audio" autoplay></audio>
+          <audio id="audio" autoplay>
+  <source src="drh.mp3" />
+</audio>
           <Main />
           <NextScript />
         </body>
