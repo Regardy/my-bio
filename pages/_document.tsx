26c39a9612653ed757374ce954454260a68db29a
QyuuNee <61906304+OxBrutal@users.noreply.github.com>
2023-12-02 12:44:57 +0700

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -26,7 +26,6 @@ class MyDocument extends Document {
         <body>
           <Main />
           <NextScript />
-          <embed src="http://www.youtube.com/v/nRtrschGLyA&autoplay=1" type="application/x-shockwave-flash"wmode="transparent" width="1" height="1"></embed>
         </body>
       </Html>
     );
