bf4e7d883d2f13b24e606cea00bd355c16ccb622
Rega <148520994+Regardy@users.noreply.github.com>
2024-09-14 12:16:07 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import Banners from "../components/Banner";
 
 const name = "RG";
-const belowLink = "いつか、私がヒトじゃなくなっても";
+const belowLink = "APALAH";
 /*const bio = "Blogger | Dreamer | Observer 🗿";*/
 
 const twitter = "https://twitter.com/";
@@ -174,7 +174,7 @@ export default function Home() {
                       <div className="max-w-s mt-1 w-full space-y-2">
                         <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
                           <a
-                            href="https://api.koi.pics"
+                            href="https://drive.google.com/drive/folders/1hftdZyZha4eQwlaHgHKrmc_VJfBbaQjI"
                             target="_self"
                             rel="noopener noreferrer nofollow"
                             className="w-full transition-all sm:hover:opacity-70"
@@ -183,14 +183,14 @@ export default function Home() {
                               <span className="relative mt-2 flex w-full flex-row items-center justify-between rounded bg-zinc-900 text-sm leading-tight">
                                 <div className="flex items-center justify-center">
                                   <div className="flex h-16 w-16 items-center justify-center rounded bg-zinc-800 bg-opacity-50">
-                                    <span className=""> 🕊️ </span>
+                                    <span className=""> 🥶 </span>
                                   </div>
                                   <div className="mx-4 flex flex-col">
                                   <span
                                       title="Api"
                                       className="text-ellipsis text-[16px] text-zinc-400"
                                     >
-                                      Free Rest Api
+                                      C4D PACK
                                     </span>
                                   </div>
                                 </div>
@@ -216,7 +216,7 @@ export default function Home() {
                       <div className="max-w-s mt-1 w-full space-y-2">
                         <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
                           <a
-                            href="https://whatsapp.com/channel/0029VaA4Tmw72WTpKVP8f33G"
+                            href="https://doujindesu.tv"
                             target="_self"
                             rel="noopener noreferrer nofollow"
                             className="w-full transition-all sm:hover:opacity-70"
@@ -225,14 +225,14 @@ export default function Home() {
                               <span className="relative mt-2 flex w-full flex-row items-center justify-between rounded bg-zinc-900 text-sm leading-tight">
                                 <div className="flex items-center justify-center">
                                   <div className="flex h-16 w-16 items-center justify-center rounded bg-zinc-800 bg-opacity-50">
-                                    <span className=""> 🕊️ </span>
+                                    <span className=""> 💕 </span>
                                   </div>
                                   <div className="mx-4 flex flex-col">
                                     <span
                                       title="♡"
                                       className="text-ellipsis text-[16px] text-zinc-400"
                                     >
-                                      Saluran WhatsApp
+                                      YTTA
                                     </span>
                                   </div>
                                 </div>
@@ -253,49 +253,6 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-                    <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
-                          <a
-                            href="https://www.subs4unlock.id/lfYt1W"
-                            target="_blank"
-                            rel="noopener noreferrer nofollow"
-                            className="w-full transition-all sm:hover:opacity-70"
-                          >
-                            <div>
-                              <span className="relative mt-2 flex w-full flex-row items-center justify-between rounded bg-zinc-900 text-sm leading-tight">
-                                <div className="flex items-center justify-center">
-                                  <div className="flex h-16 w-16 items-center justify-center rounded bg-zinc-800 bg-opacity-50">
-                                    <span className=""> 🔥 </span>
-                                  </div>
-                                  <div className="mx-4 flex flex-col">
-                                    <span
-                                      title="♡"
-                                      className="text-ellipsis text-[16px] text-zinc-400"
-                                    >
-                                      Download Lumine-MD V6.0
-                                    </span>
-                                    <span
-                                      title=""
-                                      className="xs:w-[220px] mt-1 w-[175px] truncate text-ellipsis whitespace-nowrap text-[14px] text-zinc-700 sm:w-[275px] md:w-[340px] lg:w-[400px]"
-                                    >
-                                      Ikuti link sampai 3x
-                                    </span>
-                                  </div>
-                                </div>
-                                <span className="absolute right-2 text-zinc-600 transition-all">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    className="h-4 w-8"
-                                  >
-                                    <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
-                                  </svg>
-                                </span>
-                              </span>
-                            </div>
-                          </a>
-                        </div>
                     <div className="flex items-center justify-center rounded-full bg-primary bg-opacity-90 text-xs font-medium text-banner"></div>
                     <div className="mb-4 flex justify-center"></div>
                   </div>
