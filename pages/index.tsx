06038b8604f9cfe57d8b6db13b54b124780607fa
Rega <148520994+Regardy@users.noreply.github.com>
2025-02-23 21:10:36 +0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -211,46 +211,6 @@ export default function Home() {
                         </div>
                       </div>
                     </div>
-     <div className="max-w-s mt-1 w-full space-y-2">
-                        <div className="bg-animate group mb-1 w-full cursor-pointer rounded bg-gradient-to-r from-zinc-900 via-zinc-600 to-zinc-900 text-sm text-zinc-400 shadow transition-all">
-                          <a
-                            href="https://drive.google.com/drive/folders/1hftdZyZha4eQwlaHgHKrmc_VJfBbaQjI"
-                            target="_self"
-                            rel="noopener noreferrer nofollow"
-                            className="w-full transition-all sm:hover:opacity-70"
-                          >
-                            <div>
-                              <span className="relative mt-2 flex w-full flex-row items-center justify-between rounded bg-zinc-900 text-sm leading-tight">
-                                <div className="flex items-center justify-center">
-                                  <div className="flex h-16 w-16 items-center justify-center rounded bg-zinc-800 bg-opacity-50">
-                                    <span className=""> 🥶 </span>
-                                  </div>
-                                  <div className="mx-4 flex flex-col">
-                                  <span
-                                      title="Api"
-                                      className="text-ellipsis text-[16px] text-zinc-400"
-                                    >
-                                      C4D PACK
-                                    </span>
-                                  </div>
-                                </div>
-                                <span className="absolute right-2 text-zinc-600 transition-all">
-                                  <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    viewBox="0 0 20 20"
-                                    fill="currentColor"
-                                    aria-hidden="true"
-                                    className="h-4 w-8"
-                                  >
-                                    <path d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"></path>
-                                  </svg>
-                                </span>
-                              </span>
-                            </div>
-                          </a>
-                        </div>
-                      </div>
-                    </div>
                     <div className="mb-4 flex justify-center">
                       <div className="flex items-center justify-center rounded-full bg-primary bg-opacity-90 text-xs font-medium text-banner"></div>
                       <div className="max-w-s mt-1 w-full space-y-2">
